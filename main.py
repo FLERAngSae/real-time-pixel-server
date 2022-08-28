28ba3c3946cdb4459f026503b88d5228689051a2
fleraS <82698085+fleraS@users.noreply.github.com>
2022-08-28 15:31:19 +0900

    makePixel function
    makePixel

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import socketio
+import json
 import asyncio
 sio = socketio.AsyncServer()
 app = socketio.ASGIApp(sio)
+
+def makePixel():
+    x = input()
+    y = input()
+    color = input()
+    with open("pixel_color.json") as pixel_json:
+        pixel_color = json.load(pixel_json)
+    pixel_color[x][y] = color
+    with open('pixel_color.json', 'w') as f:
+        json.dump(pixel_color, f)
+
+makePixel()
 
 @sio.on('connect')
 def connect_handler(sid, environ):
