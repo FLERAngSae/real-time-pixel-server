1886d0c62acbb736981f47235d482ba436157658
fleraS <82698085+fleraS@users.noreply.github.com>
2022-08-28 17:40:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def makePixel():
     x = input()
     y = input()
     color = input()
-    with open("pixel_color.json") as pixel_json:
+    with open("data/pixel_color.json") as pixel_json:
         pixel_color = json.load(pixel_json)
     pixel_color[x][y] = color
     with open('data/pixel_color.json', 'w') as f:
